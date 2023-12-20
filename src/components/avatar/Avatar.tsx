c2896f610ad78d3a4906e96ab025cbe96ce8075c
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-20 16:35:58 +0100

    separated avatar css

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,22 +1,11 @@
+import "./Avatar.css"
+
 const user = {
   name: "Marius Skjellerud",
   imageSrc: "/image001.png",
-  imageSize: 180,
 }
 
 const Avatar = () => {
-  return (
-    <img
-      className="avatar"
-      src={user.imageSrc}
-      alt=""
-      style={{
-        width: user.imageSize,
-        height: user.imageSize,
-        borderRadius: 100,
-        objectFit: "cover",
-      }}
-    />
-  )
+  return <img className="avatar" src={user.imageSrc} alt={`photo of ${user.name}`} />
 }
 export default Avatar
