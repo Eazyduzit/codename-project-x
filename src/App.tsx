8c6ba1711a4099b146df8394bc14da200ab5be89
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-20 16:55:49 +0100

    changed some colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Avatar from "./components/avatar/Avatar"
 function App() {
   return (
     <div className="App">
-      <Avatar />
       <h1>Hei, mitt navn er Marius!</h1>
+      <Avatar />
       <h2>Jeg er en React og Typescript utvikler, i frontend..</h2>
     </div>
   )
