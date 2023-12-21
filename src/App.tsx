52f397e5401bcaa00880d32b4e5db23d16079055
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-21 16:45:06 +0100

    added parallax component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css"
-import Avatar from "./components/avatar/Avatar"
+import Parallax from "./components/parallax/Parallax"
+// import Avatar from "./components/avatar/Avatar"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hei, mitt navn er Marius!</h1>
+      <Parallax />
+      {/* <h1>Hei, mitt navn er Marius!</h1>
       <Avatar />
-      <h2>Jeg er en React og Typescript utvikler, i frontend..</h2>
+      <h2>Jeg er en React og Typescript utvikler, i frontend..</h2> */}
     </div>
   )
 }
---
+++ src/components/parallax/Parallax.tsx
@@ -0,0 +1,4 @@
+const Parallax = () => {
+  return <div className="wrapper"></div>
+}
+export default Parallax
