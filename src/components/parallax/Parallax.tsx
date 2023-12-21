dc68b2f645ffe34d4e210569e94ba525378ecb2d
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-21 21:11:29 +0100

    added types to parallax component

--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -1,20 +1,35 @@
 import Avatar from "../avatar/Avatar"
 import "./Parallax.css"
 
-const images = {
+interface Images {
+  imgSrc1: string
+  imgSrc2: string
+  description1: string
+  description2: string
+}
+
+const images: Images = {
   imgSrc1: "./images/mountain1.jpg",
   imgSrc2: "./images/trees1.png",
   description1: "mountain range",
   description2: "pine trees",
 }
 
-const Parallax = () => {
+const Parallax: React.FC = () => {
   return (
-    <div className="ParallaxWrapper">
+    <div className="parallaxWrapper">
       <header>
-        <img className="background" src={images.imgSrc1} alt={`photo of ${images.description1}`} />
-        <img className="foreground" src={images.imgSrc2} alt={`photo of ${images.description2}`} />
-        <h1 className="title">Welcome!</h1>
+        <img
+          className="parallaxBackground"
+          src={images.imgSrc1}
+          alt={`photo of ${images.description1}`}
+        />
+        <img
+          className="parallaxForeground"
+          src={images.imgSrc2}
+          alt={`photo of ${images.description2}`}
+        />
+        <h1 className="parallaxTitle">Welcome!</h1>
       </header>
       <Avatar />
     </div>
