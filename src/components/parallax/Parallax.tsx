a1b82bfbd1ae3b65fae9f498cbc5eb0f22607656
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-21 18:32:36 +0100

    moved src and alt into object

--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -1,12 +1,19 @@
 import Avatar from "../avatar/Avatar"
 import "./Parallax.css"
 
+const images = {
+  imgSrc1: "./mountain1.jpg",
+  imgSrc2: "./trees1.png",
+  description1: "mountain range",
+  description2: "pine trees",
+}
+
 const Parallax = () => {
   return (
     <div className="ParallaxWrapper">
       <header>
-        <img className="background" src="mountain1.jpg" alt="mountain range" />
-        <img className="foreground" src="trees1.png" alt="pine trees" />
+        <img className="background" src={images.imgSrc1} alt={`photo of ${images.description1}`} />
+        <img className="foreground" src={images.imgSrc2} alt={`photo of ${images.description2}`} />
         <h1 className="title">Welcome!</h1>
       </header>
       <section>
