b1997ffe57492a704399d9678e508193799a795a
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-21 16:58:27 +0100

    added structure and images

--- src/components/parallax/Parallax.tsx
+++ src/components/parallax/Parallax.tsx
@@ -1,4 +1,42 @@
+import "./Parallax.css"
+
 const Parallax = () => {
-  return <div className="wrapper"></div>
+  return (
+    <div className="wrapper">
+      <header>
+        <img src="mountain1.jpg" alt="" />
+        <img src="trees.png" alt="" />
+        <h1>Welcome!</h1>
+      </header>
+      <section>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio aliquam consequuntur
+        obcaecati nesciunt? Architecto ab culpa reiciendis enim sed repellendus eius! Voluptatibus,
+        corrupti quisquam! Nostrum, consequatur. Atque soluta quam voluptate! Non, eaque distinctio
+        reiciendis dicta illo aperiam unde. Odio alias expedita mollitia saepe ipsum at, natus
+        cumque, unde porro totam odit consectetur sunt beatae esse harum? Neque maxime inventore
+        quas! Dolor autem eaque maiores nobis rerum atque recusandae reiciendis! Dolorem tempore
+        ullam optio odio, reprehenderit quaerat illum nulla recusandae at. Ullam voluptatibus quos
+        odio hic, quam ut quas molestiae possimus? Animi rem aliquam sequi? Quisquam aspernatur
+        perferendis quos ipsam delectus accusantium animi vel molestiae nesciunt voluptatem numquam
+        laboriosam ipsum nemo optio labore rerum fuga, voluptates iure voluptas ducimus? Repellat,
+        doloribus? Iusto, vel, magnam perferendis cum, illo debitis eveniet consequatur enim quam
+        totam asperiores aperiam possimus voluptatum quos sint! Inventore, voluptatum iste in enim
+        eveniet delectus asperiores nisi reprehenderit culpa qui? Quae corporis autem dolorem fuga
+        qui provident quisquam id, velit ex consequatur suscipit illo nulla distinctio excepturi
+        aliquid vitae praesentium delectus accusamus. Incidunt odit quam illum corrupti itaque
+        beatae fugit. Minima sed tempore magnam voluptate praesentium doloribus inventore, sunt
+        asperiores itaque quia, consequuntur quas voluptas modi dolorem iure sequi? Quasi, omnis
+        maxime facere exercitationem esse nemo ratione reprehenderit excepturi odio! Recusandae
+        error exercitationem delectus rerum molestias unde. Eaque cum ullam ut id mollitia veritatis
+        saepe. Incidunt officiis ratione deleniti aliquam provident pariatur cumque distinctio
+        nesciunt, rerum, hic id in libero? Consectetur quasi, debitis mollitia illum placeat iusto
+        magnam accusantium nihil quisquam qui aut quo numquam velit, quaerat inventore ipsam
+        corrupti fugit corporis porro. Cumque rerum eligendi mollitia fugit, ad officiis. Alias
+        mollitia beatae laborum architecto quis. Earum eaque laudantium numquam voluptates at quia
+        nobis, rem magni quae. Quo ea, dolores atque minima dolorem mollitia? Omnis et ducimus at
+        similique temporibus!
+      </section>
+    </div>
+  )
 }
 export default Parallax
