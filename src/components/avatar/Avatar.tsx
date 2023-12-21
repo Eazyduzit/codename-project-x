8ed6136cba7d3e6167eb4986ea1432e6beebc304
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-21 18:24:05 +0100

    styling and positioning

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,7 +1,7 @@
 import "./Avatar.css"
 
 const user = {
-  name: "Marius Skjellerud",
+  name: "Marius",
   imageSrc: "/image001.png",
 }
 
@@ -9,7 +9,7 @@ const Avatar = () => {
   return (
     <div className="avatarWrapper">
       <img className="avatarImage" src={user.imageSrc} alt={`photo of ${user.name}`} />
-      <p className="avatarTitle">Hei, mitt navn er Marius!</p>
+      <p className="avatarTitle">{`Hei, mitt navn er ${user.name}!`}</p>
       <p className="avatarParagraph">Jeg er en React og Typescript utvikler, i frontend..</p>
     </div>
   )
