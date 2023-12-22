9ce1d1e7eb1eecd9e456dbcbac5424d7937b91a3
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-22 13:39:23 +0100

    added first jest test

---
+++ src/components/avatar/Avatar.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import Avatar from "./Avatar"
+
+describe("Avatar component", () => {
+  const AvatarProps = {
+    name: "Marius",
+    imageSrc: "/images/image001.png",
+  }
+
+  it("renders with correct props", () => {
+    render(<Avatar {...AvatarProps} />)
+
+    // Check if the image and text elements are rendered
+    expect(screen.getByAltText(`photo of ${AvatarProps.name}`)).toBeInTheDocument()
+    expect(screen.getByText(`Hei, mitt navn er ${AvatarProps.name}!`)).toBeInTheDocument()
+    expect(
+      screen.getByText("Jeg er en React og Typescript utvikler, i frontend.."),
+    ).toBeInTheDocument()
+  })
+})
